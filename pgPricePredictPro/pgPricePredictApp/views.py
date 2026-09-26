c805f216b0c0d0f7f0588e272ae465d954a82bb6
kashyap tiwari <kashyaptiwari444@gmail.com>
2026-09-26 15:05:56 +0530

    Refactor code structure for improved readability and maintainability

--- pgPricePredictPro/pgPricePredictApp/views.py
+++ pgPricePredictPro/pgPricePredictApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+import pandas as pd
+
+
+
 # Create your views here.
 
 def predict_price(request):
+    
     return render(request, "prediction.html")
